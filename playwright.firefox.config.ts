b2d3b77050c75c59d2f16b75e39d820ed4110987
Michel Krispin <michel.krispin@tuhh.de>
2026-09-25 20:28:00 +0200

    Report Firefox failures in CI

--- playwright.firefox.config.ts
+++ playwright.firefox.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./e2e",
   workers: 1,
+  reporter: process.env.CI ? [["github"], ["line"]] : "list",
   use: { baseURL: "http://127.0.0.1:5173", browserName: "firefox" },
   webServer: {
     command: "npm run dev",
