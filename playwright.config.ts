bd45364cbd2c5005283d1bfce00c8934d4bf494e
Michel Krispin <michel.krispin@tuhh.de>
2026-09-25 18:28:43 +0200

    Expose browser failures in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,8 +4,10 @@ export default defineConfig({
   testDir: "./e2e",
   testIgnore: ["**/pages-base.spec.ts", "**/mixed-browsers.spec.ts"],
   workers: 1,
+  reporter: process.env.CI ? [["github"], ["line"]] : "list",
   use: {
     baseURL: "http://127.0.0.1:5173",
+    trace: "retain-on-failure",
     launchOptions: {
       executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH,
     },
